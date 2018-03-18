92cbf2ebf7def089099dd70e077f45e865996062
jßan <ahsan.wtc@gmail.com>
2018-03-18 23:13:58 +0100

    setting up create user function

--- functions/create-user.js
+++ functions/create-user.js
@@ -1,5 +1,21 @@
+
+const admin = require('firebase-admin')
 const createUser = (request, response) => {
-    response.send(request.body);
+    // verify user provided a phone
+    if (!request.body.phone) {
+        return response.status(422).send({ error: 'Bad Input' });
+    }
+    
+    // format the phone number to remove dashes and parens
+    const phone = String(request.body.phone).replace(/[^\d]/g, "");
+
+    // create a new user account using that phone number
+    admin.auth().createUser({ uid: phone })
+        .then(user => response.send(user))
+        .catch(error => response.status(422).send({ error }));
+
+    // respond to the user request, saying the account was made
+
 };
 
 module.exports = createUser;
